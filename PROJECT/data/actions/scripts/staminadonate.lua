d174530a9fad4d84e5fe99cd59ac65745c9fd81d
loukeira <35003597+loukeira@users.noreply.github.com>
2018-03-04 16:46:41 -0300

    stamina donate
    stamina donate

--- PROJECT/data/actions/scripts/staminadonate.lua
+++ PROJECT/data/actions/scripts/staminadonate.lua
@@ -5,7 +5,7 @@ function onUse(cid, item, frompos, item2, topos)
 
 local desc = getItemInfo(12505).description
 	local newitem = doPlayerAddItem(cid, 12505, 1)
-	doItemSetAttribute(newitem, "description", desc.." ".."Nova description")
+	doItemSetAttribute(newitem, "description", desc.." ".." Uhum SEi ")
 	doRemoveItem(item.uid, 1)	
 
 
