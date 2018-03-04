a081152f192ef11fabd0500604ebfa982dd6557c
loukeira <35003597+loukeira@users.noreply.github.com>
2018-03-04 20:11:02 -0300

    item xml
    Item xml

--- PROJECT/data/actions/scripts/staminadonate.lua
+++ PROJECT/data/actions/scripts/staminadonate.lua
@@ -1,11 +1,11 @@
-
 function onUse(cid, item, frompos, item2, topos)
-
--- getPlayerGUID(cid)
 
 local desc = getItemInfo(12505).description
 	local newitem = doPlayerAddItem(cid, 12505, 1)
-	doItemSetAttribute(newitem, "description", desc.." ".." Uhum SEi ")
+	doItemSetAttribute(newitem, "description", desc.." ".." pertence ao player "..getCreatureName(cid).." " )
+		doItemSetAttribute(newitem, "aid", getPlayerGUID(cid)) 
+
+
 	doRemoveItem(item.uid, 1)	
 
 
--- PROJECT/data/actions/scripts/staminateste.lua
+++ PROJECT/data/actions/scripts/staminateste.lua
@@ -1,11 +1,24 @@
+
 function onUse(cid, item, frompos, item2, topos)
-local itemzin = item.uid
-local coco = itemzin.description
-local hoho = tostring(coco)
-doBroadcastMessage("server "..hoho.." ")
+
+
+if item.aid == getPlayerGUID(cid) then
+   doCreatureSay(cid, "Ahhhh!!", TALKTYPE_ORANGE_1)
+   doPlayerSendTextMessage(cid,22,"Voce encheu sua Stamina!")
+
+-- if item.itemid == 12505 then
+-- doBroadcastMessage(" item ok")
+-- end
+
 
 	doPlayerSetStamina(cid, 2520)
 	doSendMagicEffect(frompos, 1)
-	-- doRemoveItem(item.uid, 1)	
+	-- doRemoveItem(item.uid, 1)
+	else
+	  doPlayerSendTextMessage(cid,22,"Esse refil de Stamina Donate nao te pertence!")
+	  	doSendMagicEffect(frompos, CONST_ME_POFF)
+
+
+	end
 return 1
 end
